4c49e20acfebc4d0402dda53f70ea91dd912bbae
zhaojiew <zhaojiew@necdcloud.cn>
2022-09-25 14:23:35 +0000

    change num of rep

---
+++ create.sh
@@ -0,0 +1,5 @@
+argocd app create test-argocd --repo https://github.com/zhaojiew05955/myargocd.git --path k8stest --dest-server https://kubernetes.default.svc --dest-namespace test-argocd
+
+
+passwd
+TZZTUkoUGwVUICEk
